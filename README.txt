6798dbc990f63f3215ad2d3bf4a0f2eea058b2fc
Spooks211 <noah.povil@gmail.com>
2022-08-04 20:08:21 +0100

    added hero scraping functionality, QoL changes

--- README.txt
+++ README.txt
@@ -1,8 +1,4 @@
-1) Ensure python is installed on your system
-2) Open a powershell window where the imagescraper.py and requirements.txt files are (Shift+Right Click in file explorer when the folder is open,
-then press open powershell window here)
-3) Pip must be installed for this to work, check whether pip is installed by typing "pip --version" on the powershell window
-4) If it is, you will see the version, if not install it by entering "py -m ensurepip --upgrade" into the powershell window
-5) Now you can install the packages that this script requires, do this by entering "pip install -r requirements.txt"
-6) The script is now ready to run, you can do this either entering "python imageScraper.py" into powershell or by opening it 
-in IDLE (or your favourite code editor) and running it through there.
+1)Open the script folder
+2)Run imageScraper.exe
+Please note the rest of the files inside script are necessary for it to function,
+as it is a compiled python file.
--- imageScraper.py
+++ imageScraper.py
@@ -1,15 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = input('Please enter a url \n(If using CMD and not Powershell paste is CTRL+SHIFT+V)\n--> ')
+while True:
+    URL = input('Please enter a url \n---> ')
 
-r = requests.get(URL) # retrieves the page data
-soup = BeautifulSoup(r.content, 'html5lib') #converts raw data to DOM
+    r = requests.get(URL) # retrieves the raw page data
+    soup = BeautifulSoup(r.content, 'html5lib') #converts raw data to DOM
 
-for row in soup.findAll('div', attrs = {'class' : 'Panel-img'}): #loops through all mentions of the css class panel-img
-    image = {} #empty obj to store data in
-    image['source'] = row.img['src'] #attaches source of each image to obj
-    image['alt'] = row.img['alt'] #attaches alt text of each image to obj
-    print(image)
+    for row in soup.findAll('div', attrs = {'class' : 'Hero-figure'}): #hero images use a different class name, this is for that
+        hero = {}
+        hero['hero source'] = row.img['src']
+        hero['hero alt'] = row.img['alt']
+        print(hero)
 
-exit = input ("press any key to exit") # this is needed so the command line doesn't immediately close once the script has run
+    for row in soup.findAll('div', attrs = {'class' : 'Panel-img'}): #loops through all mentions of the css class panel-img (cards etc)
+        image = {} #empty object to store source and alt in
+        image['source'] = row.img['src'] #attaches source of each image to object
+        image['alt'] = row.img['alt'] #attaches alt text of each image to object
+        print(image)
+
+    exitOrRunAgain = input ("Would you like to run the script again? y/n \n IF YOU WANT TO KEEP THE DATA ON SCREEN TO WORK ON IT DO NOT PRESS ANYTHING UNTIL YOU'RE DONE\n---> ")
+    exitOrRunAgain = exitOrRunAgain.lower()
+
+    if exitOrRunAgain == 'y':
+        continue
+
+    elif exitOrRunAgain == 'n':
+        break
+
+    else:
+        break

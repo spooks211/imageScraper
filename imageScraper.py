25421d6d9f4a7b51473046c3ff12ff835357322a
Spooks211 <noah.povil@gmail.com>
2022-08-04 19:09:41 +0100

    tidied up code, added a compiled .exe version of the script

--- imageScraper.py
+++ imageScraper.py
@@ -3,11 +3,13 @@ from bs4 import BeautifulSoup
 
 URL = input('Please enter a url \n(If using CMD and not Powershell paste is CTRL+SHIFT+V)\n--> ')
 
-r = requests.get(URL)
-soup = BeautifulSoup(r.content, 'html5lib')
+r = requests.get(URL) # retrieves the page data
+soup = BeautifulSoup(r.content, 'html5lib') #converts raw data to DOM
 
-for row in soup.findAll('div', attrs = {'class' : 'Panel-img'}):
-    image = {}
-    image['source'] = row.img['src']
-    image['alt'] = row.img['alt']
+for row in soup.findAll('div', attrs = {'class' : 'Panel-img'}): #loops through all mentions of the css class panel-img
+    image = {} #empty obj to store data in
+    image['source'] = row.img['src'] #attaches source of each image to obj
+    image['alt'] = row.img['alt'] #attaches alt text of each image to obj
     print(image)
+
+exit = input ("press any key to exit") # this is needed so the command line doesn't immediately close once the script has run
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 import py2exe
 
-setup (console=[imageScraper.py])
+setup (console=['imageScraper.py'])
